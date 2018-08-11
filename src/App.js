ed6db2be5761bf9f8c7ca549a1ffa3da1e26ec14
jayaprakashponnusamy <jaypeecomrade@gmail.com>
2018-08-11 12:15:40 +0530

    commited upto the user routing configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Link, NavLink, Redirect } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import './App.css';
-const user = ({ match }) => {
-  return (<p> Welcomt user {match.params.username}</p>)
+const User = (params) => {
+  return (<p> Welcomt user {params.username}</p>)
 }
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
 
+  loggedInHandle = () => {
+    this.setState({loggedIn: true});
+  }
   render() {
 
     return (
@@ -15,7 +21,13 @@ class App extends Component {
           <ul>
             <li><NavLink to="/" class="link" activeClass="active">Home</NavLink></li>
             <li><NavLink to="/about" class="link">About</NavLink></li>
+            <li>User<ul>
+            <li><NavLink to="/user/raja" class="link">Raja</NavLink></li>
+            <li><NavLink to="/user/rahman" class="link">Rahman</NavLink></li>
+            </ul></li>
           </ul>
+          <input type="button" value="Log in" onClick={this.loggedInHandle.bind(this)}/>
+          <p>{this.state.loggedIn?'true':'false'}</p>
           <Route path="/" exact strict render={
             () => {
               return (<p> This is the home page</p>);
@@ -26,7 +38,13 @@ class App extends Component {
               return (<p> This is the about page </p>);
             }
           } />
-          <Route path="/user/:username" exact strict component={user} />
+          
+          <Route path="/user/:username" exact strict render={
+            ({match})=>{
+              return (
+              this.state.loggedIn ?(<User username={match.params.username}/>):(<Redirect to="/about"/>)
+            )}
+          } />
         </div>
       </Router>
     );
